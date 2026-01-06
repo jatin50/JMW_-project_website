ebc2f9544654389c6acea10473222cc71f86753c
jatin50 <jainjatin2006@gmail.com>
2026-01-06 17:32:31 +0530

    feat: add models for Category, Product, and User with necessary fields and validations

---
+++ backend/models/product.models.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    imageUrl:{
+        type:String,
+        required:true,
+    },
+    fabric:{
+        type:String,
+        required:true,
+    },
+    color:{
+        type:Array,
+        required:true,
+    },
+    size:{
+        type:Array,
+        required:true,
+    },
+    gsm:{
+        type:Number,
+        required:true,
+    },
+    quantity:{
+        type:Number,
+        required:true,
+        default:1,
+    },
+},{ timestamps: true });
+
+ export const Product = mongoose.model("Product", productSchema);
