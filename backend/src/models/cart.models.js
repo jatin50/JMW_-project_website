0de79f1ae5c79fba4df66bc2ab9129cdb3fbf2fd
jatin50 <jainjatin2006@gmail.com>
2026-02-11 18:19:24 +0530

    feat: add cart model with user and product references

---
+++ backend/src/models/cart.models.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const cartschema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    products:[{
+        productId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Product",
+            required:true
+        },
+        quantity:{
+            type:Number,
+            default:1
+        }
+    }]
+},{timestamps: true})
+export const Cart = mongoose.model("Cart", cartschema);
