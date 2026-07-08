05fde27ad1154cc483739721bdf40fc2b43c97f3
jatin50 <jainjatin2006@gmail.com>
2026-07-08 17:25:11 +0530

    feat: update product routes; consolidate imports and improve route organization

--- backend/routes/Product.routes.js
+++ backend/routes/Product.routes.js
@@ -1,21 +1,22 @@
 import { Router } from "express";
-import { UploadProduct } from "../controllers/Product.controllers.js";
+import { UploadProduct, getProducts } from "../controllers/Product.controllers.js";
 import { upload } from "../middleware/multer.middleware.js";
-import { RegisterUser } from "../controllers/User.controller.js";
-import { LoginUser,LogoutUser } from "../controllers/User.controller.js";
+import { RegisterUser, LoginUser, LogoutUser, refreshAccessToken, ChangePassword, GetUser } from "../controllers/User.controller.js";
 import { VerifyJwt } from "../middleware/auth.middleware.js";
 const router = Router();
+
+// product routes
 router.route("/upload-product").post(
   upload.single("imageUrl"),
   UploadProduct
 );
-router.route("/register").post(
-  upload.single("imageUrl"),
-  RegisterUser
-)
+router.route("/").get(getProducts)
+
+router.route("/register").post(RegisterUser)
 router.route("/login").post(LoginUser)
-router.route("/logout").post(
-  VerifyJwt,
-  LogoutUser
-)
+router.route("/logout").post(VerifyJwt, LogoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(VerifyJwt, ChangePassword)
+router.route("/me").get(VerifyJwt, GetUser)
+
 export default router;
